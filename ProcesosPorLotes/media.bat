1df49d1c15ed6b41e98bf80b486c677c719b1f87
sgonzalezm78 <150674484+sgonzalezm78@users.noreply.github.com>
2023-11-14 14:01:17 +0100

    Update media.bat

--- ProcesosPorLotes/media.bat
+++ ProcesosPorLotes/media.bat
@@ -3,8 +3,5 @@ REM Crea una variable que se encargue de crear dentro del perfil del usuario act
 REM 
 REM Autor: Santiago González Mangut
 REM Fecha: 14-11-2023
-set /a numero1=%1
-set /a numero2=%2
-set /a numero3=%3
-set /a media=(%numero1% + %numero2% + %numero3%) / 3
-echo La media de %numero1%, %numero2% y %numero3% es: %media%
+set /a media=(%1 + %2 + %3) / 3
+echo La media de %1, %2 y %3 es: %media%
