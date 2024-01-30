3c2f9b73e801b3031c760ebd0ec2e7eaee2dc58c
sgonzalezm78 <150674484+sgonzalezm78@users.noreply.github.com>
2024-01-30 13:54:24 +0100

    Add files via upload

---
+++ Shell Scripts/FiveOrMore.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Autor: Santiago
+# Fecha: 29-01-2024
+
+if [ $# -ne 2 ]; then
+	echo "numero de parametro incorrecto"
+	exit
+fi
+
+if [ -f $1 ]; then
+	echo  "El fichero ya existe" 
+	exit
+fi
+
+if [ -d $1  ]; then
+	echo "El directorio existe"
+fi
